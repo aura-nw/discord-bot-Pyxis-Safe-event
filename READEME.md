f4877f6eab3a1477047aa7965b9bf53b6a4b2029
nam3t <namnh2711@gmail.com>
2022-09-16 17:35:14 +0700

    add bot event Pyxis Safe

---
+++ READEME.md
@@ -0,0 +1,11 @@
+# Discord bot for Pyxis Safe event
+This bot will be listen to a specific event channel from a member, if the message match the pattern. The bot will reply to all wallet addresses that linked with that member.
+
+## Dev environment
+- node version: v14.16.0
+- npm version: 6.14.11
+
+## Setup
+- Copy file `config.example.json`
+- Replace with your config
+  - *Note: `data` field are list linked user id discord 
---
+++ app.js
@@ -0,0 +1,49 @@
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const { token, data } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.DirectMessages,
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildBans,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent], 'partials': [Partials.Channel] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('messageCreate', async (msg) => {
+	try {
+		const message = msg.content.toLowerCase();
+		// console.log(message);
+
+		const userId = msg.author.id;
+		// console.log(userId);
+
+		if (message.startsWith('i have done my pyxis safe task')) {
+			const listLinkedId = data.find(element => element.find(e => e == userId));
+			let replyMsg = 'It\'s your turn, guys!!! ';
+			if (listLinkedId.includes(userId)) {
+				for (const user in listLinkedId) {
+					if (Object.hasOwnProperty.call(listLinkedId, user)) {
+						const el = listLinkedId[user];
+						if (el == userId) continue;
+						replyMsg = replyMsg.concat(`<@${el}> `);
+					}
+				}
+			}
+
+			// console.log(replyMsg);
+
+			msg.reply(replyMsg);
+		}
+	}
+	catch (error) {
+		console.log(`${error.name}: ${error.message}`);
+	}
+});
+
+// Login to Discord with your client's token
+client.login(token);
