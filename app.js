48ae3347f0151b8177bf319b1fe74de9644c4f0f
kienvc <vuchikien269@gmail.com>
2022-10-03 14:45:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -18,18 +18,17 @@ client.on('messageCreate', async (msg) => {
 	try {
 		const message = msg.content.toLowerCase();
 
-		const userId = msg.author.tag;
-		
-		if (message.startsWith('ping my partners')) {
-			const listLinkedId = data.find(element => element.find(e => e === userId));
+		const userId = msg.author.id;
+
+		if (message.startsWith('i have done my pyxis safe task')) {
+			const listLinkedId = data.find(element => element.find(e => e == userId));
 			let replyMsg = 'It\'s your turn, guys!!! ';
 			if (listLinkedId.includes(userId)) {
-			for (const user in listLinkedId) {
+				for (const user in listLinkedId) {
 					if (Object.hasOwnProperty.call(listLinkedId, user)) {
 						const el = listLinkedId[user];
 						if (el == userId) continue;
-						let f = client.users.cache.find(userf => userf.tag === el);
-						replyMsg = replyMsg.concat(`<${f}> `);
+						replyMsg = replyMsg.concat(`<@${el}> `);
 					}
 				}
 			}
